c499a86a4742c2a23345c9fc137ddfd3c22c7a49
lahodny <axareen44@gmail.com>
2021-11-10 16:39:22 +0100

    first commit

---
+++ log/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    a="xx"
+    return HttpResponse("Hello, world. You're at the log index.")
